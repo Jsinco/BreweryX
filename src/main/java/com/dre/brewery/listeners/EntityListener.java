4b69c578f395556acd1992b0d193674c1c0a8b32
Mitality <96748385+Mitality@users.noreply.github.com>
2024-12-11 21:57:31 +0100

    Fix barrels being destroyed when hit by a WindCharge

--- src/main/java/com/dre/brewery/listeners/EntityListener.java
+++ src/main/java/com/dre/brewery/listeners/EntityListener.java
@@ -74,6 +74,7 @@ public class EntityListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onExplode(EntityExplodeEvent event) {
+		if (causedByWindCharge(event)) return; // Fixes barrels being destroyed when hit by a WindCharge
 		ListIterator<Block> iter = event.blockList().listIterator();
 		if (!iter.hasNext()) return;
 		List<BarrelDestroyEvent> breakEvents = new ArrayList<>(6);
@@ -115,6 +116,41 @@ public class EntityListener implements Listener {
 		if (Barrel.get(event.getBlock()) != null) {
 			event.setCancelled(true);
 		}
+	}
+
+	/**
+	 * Temporary utility method to determine if the given event was caused by a wind charge
+	 * @param event An instance of EntityExplodeEvent that should be analyzed
+	 * @return A boolean representing if the given event was caused by a wind charge
+	 */
+	private boolean causedByWindCharge(EntityExplodeEvent event) {
+
+		EntityType type = event.getEntityType();
+
+		/*
+		 * As of BreweryX v3.4.4, BX uses the Spigot API of 1.20.2, which doesn't include the WindCharge as an EntityType.
+		 * For that reason, we turn our approach around and check for all other entities that could cause this event.
+		 */
+
+		if (type == EntityType.ENDER_CRYSTAL) return false;
+		if (type == EntityType.MINECART_TNT) return false;
+		if (type == EntityType.WITHER_SKULL) return false;
+		if (type == EntityType.ENDER_DRAGON) return false;
+		if (type == EntityType.PRIMED_TNT) return false;
+		if (type == EntityType.FIREBALL) return false;
+		if (type == EntityType.CREEPER) return false;
+		if (type == EntityType.PLAYER) return false;
+		if (type == EntityType.WITHER) return false;
+		if (type == EntityType.GHAST) return false;
+
+		return true; // Event most likely caused by a WindCharge
+
+		/*
+		 * Note that, since WindCharges have the ability to modify BlockStates (e.g. flip trapdoors they hit), we sadly
+		 * can't just check for event.blockList(), as they provide one too and checking if those blocks are destroyed
+		 * could only be done after the event has been successfully executed. Life isn't that easy at times :/
+		 */
+
 	}
 
 }
