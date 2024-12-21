8692ded8c578c51d871b2865d21c50897f4bee49
Jsinco <jonahmarksinclair@gmail.com>
2024-12-21 05:53:40 -0500

    so annoying

--- build.gradle.kts
+++ build.gradle.kts
@@ -207,7 +207,7 @@ hangarPublish {
         platforms {
             register(Platforms.PAPER) {
                 jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-                platformVersions.set(listOf("1.13.x-1.21.x"))
+                platformVersions.set(listOf("1.13.x", "1.14.x", "1.15.x", "1.16.x", "1.17.x", "1.18.x", "1.19.x", "1.20.x", "1.21.x"))
             }
         }
         changelog.set(readChangeLog())
